aa74873d8fb0615fdc6af3a061caa278fbb36116
Sharee Sivaraya <ss3114@cam.ac.uk>
2024-07-13 23:55:29 +0100

    FlatList Created

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,12 +1,22 @@
 import { StyleSheet, Image } from 'react-native';
 import { Text, View } from '@/src/components/Themed';
 import Colors from '@/src/constants/Colors';
+import { Product } from '../types';
 
+export const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
 
-const ProductListItem = ({ product }) => {
+type ProductListItemProps = {
+    product: Product;
+}
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
   <View style={styles.container}>
-    <Image source={{ uri : product.image}} style={styles.image} />
+    <Image 
+    source={{ uri : product.image || defaultPizzaImage}} 
+    style={styles.image} 
+    resizeMode='contain'
+    />
     <Text style={styles.title}>{product.name}</Text>
     <Text>${product.price}</Text>
   </View>
@@ -20,6 +30,9 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     padding: 10,
     borderRadius: 20,
+    flex: 1,
+    maxWidth: '50%',
+
 
   },
   title: {
@@ -32,7 +45,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   image: {
-    width: '50%',
+    width: '100%',
     aspectRatio: 1,
     alignSelf: 'center'
   }
