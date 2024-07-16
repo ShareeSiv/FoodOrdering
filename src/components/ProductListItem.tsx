b29115ac90eb39c1053ba57532136894fb0f99d7
Sharee Sivaraya <ss3114@cam.ac.uk>
2024-07-16 21:35:33 +0100

    Admin Site

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -2,7 +2,7 @@ import { StyleSheet, Image , Pressable } from 'react-native';
 import { Text, View } from '@/src/components/Themed';
 import Colors from '@/src/constants/Colors';
 import { Product } from '../types';
-import { Link } from 'expo-router';
+import { Link, useSegments } from 'expo-router';
 export const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
 
 type ProductListItemProps = {
@@ -10,8 +10,11 @@ type ProductListItemProps = {
 }
 
 const ProductListItem = ({ product }: ProductListItemProps) => {
+  const segments = useSegments();
+  
+
   return (
-  <Link href={`/menu/${product.id}`} asChild>
+  <Link href={`/${segments[0]}/menu/${product.id}`} asChild>
   <Pressable style={styles.container}>
     <Image 
     source={{ uri : product.image || defaultPizzaImage}} 
