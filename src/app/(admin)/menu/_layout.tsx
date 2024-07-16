a9310874428ad8c9ab07ddc6ef99f4c2ba3ea554
Sharee Sivaraya <ss3114@cam.ac.uk>
2024-07-17 00:11:16 +0100

    Update an Delete Product UI.

--- src/app/(admin)/menu/_layout.tsx
+++ src/app/(admin)/menu/_layout.tsx
@@ -5,21 +5,17 @@ import Colors from "@/src/constants/Colors";
 
 export default function MenuStack () {
     return ( 
-    <Stack 
-        screenOptions={{
-            
-        }}
-    >
+    <Stack>
         <Stack.Screen 
-          name="[id]" 
+          name="index" 
           options={{
             title:"Menu",
             headerRight: () => (
-              <Link href="/" asChild>
+              <Link href="/(admin)/menu/create" asChild>
                 <Pressable>
                   {({ pressed }) => (
                     <FontAwesome
-                      name="pencil"
+                      name="plus-square-o"
                       size={25}
                       color={Colors.light.tint}
                       style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
